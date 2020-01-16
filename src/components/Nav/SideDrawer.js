498baefc0b2c49bb5c9998ab08e35d46256b3ebb
Jacob Watton <jacobwatton@Jacobs-MacBook-Pro.local>
2020-01-15 23:19:45 -0500

    styling for nav slider

--- src/components/Nav/SideDrawer.js
+++ src/components/Nav/SideDrawer.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import './SideDrawer.css';
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './SideDrawer.css'
 import StudentLinks from './StudentLinks'
 import TutorLinks from './TutorLinks'
 import AdminLinks from './AdminLinks'
 
 const SideDrawer = props => {
-	let drawerClasses = 'side-drawer';
+	let drawerClasses = 'side-drawer'
 	if (props.show) {
-		drawerClasses = 'side-drawer open';
+		drawerClasses = 'side-drawer open'
 	}
 	return (
-		<nav className={drawerClasses}>
-			    {props.user === "student" && <StudentLinks toggle={props.toggle}/>}
-                {props.user === "tutor" && <TutorLinks toggle={props.toggle}/>}
-                {props.user === "admin" && <AdminLinks toggle={props.toggle} />}
-		</nav>
-	);
-};
+		<React.Fragment>
+			<nav className={drawerClasses}>
+				<div className="side-drawer-logo">
+					<NavLink onClick={props.toggle} to="/dashboard" className="nav-link">
+						THRIVE LOGO
+                	</NavLink>
+				</div>
+				{props.user === "student" && <StudentLinks toggle={props.toggle} />}
+				{props.user === "tutor" && <TutorLinks toggle={props.toggle} />}
+				{props.user === "admin" && <AdminLinks toggle={props.toggle} />}
+			</nav>
+		</React.Fragment>
+	)
+}
 
-export default SideDrawer;
+export default SideDrawer
